b5b3a8f5019eeb9d7bef9e55d7a3bf3256632952
Seyeon <47477205+SeyeonJang@users.noreply.github.com>
2023-03-27 12:53:39 +0900

    [Bronze V] Title: 사칙연산, Time: 120 ms, Memory: 13252 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,12 @@
+import java.util.*
+fun main() {
+    val sc = Scanner(System.`in`) 
+    val num1 = sc.nextInt()
+    val num2 = sc.nextInt()
+
+    println(num1+num2)
+    println(num1-num2)
+    println(num1*num2)
+    println(num1/num2)
+    println(num1%num2)
+}
